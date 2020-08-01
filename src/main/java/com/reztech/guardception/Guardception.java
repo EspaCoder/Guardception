fb98ae7be2043eb46ab93774c41e41f8a09009f2
EspaCoder <erik.mb97@yahoo.es>
2020-08-01 19:05:55 +0200

    - Updated POM with 1.15.2 compatibility.                     - Created GuardceptionPermissions to provide simple helper functions to add, delete or check region permissions.                     - Overrided the onCommand method to provide functionality for the /gception (add | del | check) [player] [region] command.                     - Created a PlayerCommandPreprocessEvent listener in order to let players define regions only if they have the permission to do it inside the regions of the selected area.                     - Updated plugin.yml

--- src/main/java/com/reztech/guardception/Guardception.java
+++ src/main/java/com/reztech/guardception/Guardception.java
@@ -1,13 +1,49 @@
 package com.reztech.guardception;
 
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 
-public class Guardception extends JavaPlugin {
+public class Guardception extends JavaPlugin implements Listener {
+
+    private GuardceptionPermissions gcpermissions;
+    private WorldGuard worldguardInstance;
 
     @Override
     public void onEnable() {
+        this.gcpermissions = new GuardceptionPermissions();
+        gcpermissions.init();
+        worldguardInstance = WorldGuard.getInstance();
+        Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().log(Level.INFO, "Guardception successfully enabled!");
     }
 
@@ -15,4 +51,104 @@ public class Guardception extends JavaPlugin {
     public void onDisable() {
         getLogger().log(Level.INFO, "Guardception successfully disabled!");
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("gception")) {
+            if (args.length < 3) {
+                sender.sendMessage(Color.RED + "Invalid number of arguments!");
+                return false;
+            }
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (player.hasPermission("guardception.subreg")) {
+                    String option = args[0];
+                    String targetPlayer = args[1];
+                    String regionName = args[2];
+                    World currentWorld = BukkitAdapter.adapt(player.getWorld());
+                    Map<String, ProtectedRegion> regions = worldguardInstance.getPlatform().getRegionContainer().get(currentWorld).getRegions();
+                    // Check if the region exists
+                    if (!regions.containsKey(regionName)) {
+                        player.sendMessage(ChatColor.RED + regionName + " region does not exist!");
+                        return false;
+                    }
+                    boolean success;
+                    if (option.equalsIgnoreCase("add")) {
+                        success = gcpermissions.addPlayerPermissionInRegion(targetPlayer, regionName);
+                        if (success)
+                            player.sendMessage(ChatColor.AQUA + "Permission to create regions inside " + regionName + " given to " + targetPlayer);
+                        else
+                            player.sendMessage(ChatColor.RED + "Something went wrong while giving " + targetPlayer + " permision to create regions inside " + regionName);
+                    }
+                    else if (option.equalsIgnoreCase("del")) {
+                        success = gcpermissions.delPlayerPermissionInRegion(targetPlayer, regionName);
+                        if (success)
+                            player.sendMessage(ChatColor.AQUA + "Permission to create regions inside " + regionName + " denied to " + targetPlayer);
+                        else
+                            player.sendMessage(ChatColor.RED + "Something went wrong while denying " + targetPlayer + " permision to create regions inside " + regionName);
+                    }
+                    else if (option.equalsIgnoreCase("check")) {
+                        boolean hasPermission = gcpermissions.hasPlayerPermissionInRegion(targetPlayer, regionName);
+                        String doesOrNot = hasPermission ? "does" : "does not";
+                        ChatColor color = hasPermission ? ChatColor.AQUA : ChatColor.RED;
+                        player.sendMessage(color + targetPlayer + " " + doesOrNot + " have permission to create regions inside " + regionName);
+                    }
+                }
+                else
+                    player.sendMessage(ChatColor.RED + "You do not have permission to do such an action!");
+            }
+        }
+        return true;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void commandInterception(PlayerCommandPreprocessEvent event) {
+        String command = event.getMessage();
+        if (command.length() >= 3) {
+            command = command.substring(1);
+            String[] args = command.split(" ");
+            // Only perform logic if it is the region (rg) command
+            if ((args[0].equalsIgnoreCase("region") || args[0].equalsIgnoreCase("rg")) && args[1].equalsIgnoreCase("define")) {
+                Player player = event.getPlayer();
+                WorldEdit worldEditInstance = WorldEdit.getInstance();
+                LocalSession session = worldEditInstance.getSessionManager().get(BukkitAdapter.adapt(player));
+                Region selection;
+                try {
+                    selection = session.getSelection(session.getSelectionWorld());
+                }
+                catch (IncompleteRegionException e) {
+                    player.sendMessage(ChatColor.RED + "Complete the region selection before creating a region!");
+                    return;
+                }
+                RegionContainer regionContainer = worldguardInstance.getPlatform().getRegionContainer();
+                RegionManager manager = regionContainer.get(BukkitAdapter.adapt(player.getWorld()));
+                int xMin = selection.getMinimumPoint().getBlockX();
+                int zMin = selection.getMinimumPoint().getBlockZ();
+                int xMax = selection.getMaximumPoint().getBlockX();
+                int zMax = selection.getMaximumPoint().getBlockZ();
+                int y = selection.getMaximumPoint().getY();
+                Set<String> regions = new HashSet<String>();
+                for (int x = xMin; x <= xMax; ++x) { //Check all the blocks inside the selection to get all the overlapping regions
+                    for (int z = zMin; z <= zMax; ++z) {
+                        BlockVector3 block = BlockVector3.at(x, y, z);
+                        List<String> regionList = manager.getApplicableRegionsIDs(block);
+                        // If no region appears, then it is the __global__ region
+                        if (regionList.isEmpty())
+                            regions.add("__global__");
+                        else
+                            regions.addAll(regionList);
+                    }
+                }
+                for (String region : regions) {
+                    // If the player has no permission to edit one of the overlapping regions, the region cannot be defined
+                    if (!gcpermissions.hasPlayerPermissionInRegion(player.getName(), region)) {
+                        player.sendMessage(ChatColor.RED + "You cannot create a region here because you have no permission to define a region inside " + region + " region!");
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
 }
---
+++ src/main/java/com/reztech/guardception/GuardceptionPermissions.java
@@ -0,0 +1,95 @@
+package com.reztech.guardception;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+
+public class GuardceptionPermissions {
+
+    // private Map<String, Set<String>> permissions;
+    private FileConfiguration regionalPermissions;
+    private JavaPlugin guardceptionPlugin;
+    private final String filePath = "plugins/Guardception/regionalPermissionsFile.yml";
+
+
+    public GuardceptionPermissions() {
+        this.guardceptionPlugin = JavaPlugin.getPlugin(Guardception.class);
+    }
+
+    public boolean init() {
+        JavaPlugin guardceptionPlugin = JavaPlugin.getPlugin(Guardception.class);
+        File dataFolder = new File("plugins/Guardception");
+        if (!dataFolder.exists())
+            dataFolder.mkdir();
+        File regionalPermissionsFile = new File(filePath);
+        if (!regionalPermissionsFile.exists()) {
+            try {
+                regionalPermissionsFile.createNewFile();
+            }
+            catch (IOException e) {
+                guardceptionPlugin.getLogger().log(Level.SEVERE, "Could not create regionalPermissionsFile.yml under plugins/Guardception");
+                return false;
+            }
+        }
+        this.regionalPermissions = new YamlConfiguration();
+        try {
+            regionalPermissions.load(regionalPermissionsFile);
+        } catch (IOException e) {
+            guardceptionPlugin.getLogger().log(Level.SEVERE, "Could not load config file " + filePath);
+            return false;
+        } catch (InvalidConfigurationException e) {
+            guardceptionPlugin.getLogger().log(Level.SEVERE, "File in " + filePath + " is corrputed or invalid");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasPlayerPermissionInRegion(String player, String region) {
+        if (regionalPermissions == null) {
+            guardceptionPlugin.getLogger().log(Level.SEVERE, "Guardception has not been intiallized!");
+            return false;
+        }
+        List<String> permittedRegions = regionalPermissions.getStringList(player);
+        return permittedRegions.contains(region);
+    }
+
+    public boolean addPlayerPermissionInRegion(String player, String region) {
+        List<String> permittedRegions = regionalPermissions.getStringList(player);
+        if (!permittedRegions.contains(region))
+            permittedRegions.add(region);
+        regionalPermissions.set(player, permittedRegions);
+        try {
+            regionalPermissions.save(filePath);
+        }
+        catch (IOException e) {
+            guardceptionPlugin.getLogger().log(Level.SEVERE, "Could not write to file " + filePath);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean delPlayerPermissionInRegion(String player, String region) {
+        List<String> permittedRegions = regionalPermissions.getStringList(player);
+        if (permittedRegions.contains(region))
+            permittedRegions.remove(region);
+        regionalPermissions.set(player, permittedRegions);
+        try {
+            regionalPermissions.save(filePath);
+        }
+        catch (IOException e) {
+            guardceptionPlugin.getLogger().log(Level.SEVERE, "Could not write to file " + filePath);
+            return false;
+        }
+        return true;
+    }
+
+}
